42fa5e7cd83842fbfa0f387a955780072a92b5ec
Atakan Karagül <ata2014kara2@gmail.com>
2023-04-04 20:54:42 +0300

    BubbleManager: refactor

--- BubbleManager.cs
+++ BubbleManager.cs
@@ -1,143 +1,153 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class BubbleManager : MonoBehaviour
-{
-    [SerializeField] private RectTransform _textRectTransform;
-    [SerializeField] private RectTransform _rectTransform;
+public class BubbleManager : MonoBehaviour {
+    [SerializeField] RectTransform _textRectTransform;
+    [SerializeField] RectTransform _rectTransform;
 
-    [SerializeField] private float _rotationSpeed;
-    [SerializeField] private float _positionSpeed;
+    [SerializeField] float _rotationSpeed;
+    [SerializeField] float _positionSpeed;
 
-    [SerializeField] private BubbleHorizontal _horizontal;
-    [SerializeField] private BubbleVertical _vertical;
-
-    private float _rotationT;
-    private Vector3 _rotationStart;
-    private Vector3 _rotationEnd;
-
-    private float _positionT;
-    private Vector3 _positionStart;
-    private Vector3 _positionEnd;
-
-    public BubbleHorizontal horizontal
-    {
-        get => _horizontal;
-        set
-        {
-            _horizontal = value;
-            if (value == BubbleHorizontal.Left)
-                _rotationEnd.y = 180;
-            else
-                _rotationEnd.y = 0;
-            _rotationStart = _rectTransform.localRotation.eulerAngles;
-            _rotationT = 0;
-            _chancingRotation = true;
-        }
-    }
-    public BubbleVertical vertical
-    {
-        get => _vertical;
-        set
-        {
-            _vertical = value;
-            if (value == BubbleVertical.Bottom)
-                _rotationEnd.x = 180;
-            else
-                _rotationEnd.x = 0;
-            _rotationStart = _rectTransform.localRotation.eulerAngles;
-            _rotationT = 0;
-            _chancingRotation = true;
-        }
-    }
-
-    public Vector3 positionDestination
-    {
-        get => _positionEnd;
-        set
-        {
-            _positionStart = _rectTransform.localPosition;
-            _positionEnd = value;
-            _positionT = 0;
-            _chancingPosition = true;
-        }
-    }
-
-    private bool _chancingRotation = false;
-    private bool _chancingPosition = false;
-
-    void Start()
-    {
-        float __x = 180;
-        float __y = 180;
-        if (_vertical == BubbleVertical.Top)
-            __x = 0;
-        if (_horizontal == BubbleHorizontal.Right)
-            __y = 0;
-        _rectTransform.localRotation = Quaternion.Euler(__x, __y, 0);
-        _textRectTransform.localRotation = Quaternion.Euler(__x, __y, 0);
-    }
-
-    void Update()
-    {
-        if (_chancingRotation)
-            ChangeRotation();
-        if (_chancingPosition)
-            ChangePosition();
-    }
-
-    private void ChangeRotation()
-    {
-        if (_rotationT > 1)
-        {
-            _chancingRotation = false;
-            return;
-        }
-        if (_rotationT > .5f)
-            _textRectTransform.localRotation = Quaternion.Euler(_rotationEnd);
-        _rectTransform.localRotation = Quaternion.Euler(Vector3.Lerp(_rotationStart, _rotationEnd, _rotationT));
-        _rotationT += Time.deltaTime * _rotationSpeed;
-    }
-
-    private void ChangePosition()
-    {
-        if (_positionT > 1)
-        {
-            _chancingPosition = false;
-            return;
-        }
-        _rectTransform.localPosition = Vector3.Lerp(_positionStart, _positionEnd, _positionT);
-        _positionT += Time.deltaTime * _positionSpeed;
-    }
-
-    public void SetVerticalBottom() { this.vertical = BubbleVertical.Bottom; }
-    public void SetVerticalTop() { this.vertical = BubbleVertical.Top; }
+    [SerializeField] BubbleHorizontal _horizontal;
     public void SetHorizantalLeft() { this.horizontal = BubbleHorizontal.Left; }
     public void SetHorizantalRight() { this.horizontal = BubbleHorizontal.Right; }
+
+    [SerializeField] BubbleVertical _vertical;
+    public void SetVerticalBottom() { this.vertical = BubbleVertical.Bottom; }
+    public void SetVerticalTop() { this.vertical = BubbleVertical.Top; }
+
+    float rotationTime;
+    Vector3 rotationStartAngles;
+    Vector3 rotationTargetAngles;
+
+    float positionTime;
+    Vector3 positionStart;
+    Vector3 positionTarget;
+
+    bool changingRot = false;
+    bool changingPos = false;
+
+    public Vector3 positionDestination {
+        get => positionTarget;
+        set {
+            positionStart = _rectTransform.localPosition;
+            positionTarget = value;
+            positionTime = 0;
+            changingPos = true;
+        }
+    }
+
+    public BubbleHorizontal horizontal {
+        get => _horizontal;
+        set {
+            _horizontal = value;
+
+            if (value == BubbleHorizontal.Left) {
+                rotationTargetAngles.y = 180;
+            } else {
+                rotationTargetAngles.y = 0;
+            }
+
+            rotationStartAngles = _rectTransform.localRotation.eulerAngles;
+            rotationTime = 0;
+
+            changingRot = true;
+        }
+    }
+
+    public BubbleVertical vertical {
+        get => _vertical;
+        set {
+            _vertical = value;
+
+            if (value == BubbleVertical.Bottom) {
+                rotationTargetAngles.x = 180;
+            } else {
+                rotationTargetAngles.x = 0;
+            }
+
+            rotationStartAngles = _rectTransform.localRotation.eulerAngles;
+            rotationTime = 0;
+
+            changingRot = true;
+        }
+    }
 
     public void SetPosition(string vector) {
         var x = vector.Split("|");
         SetPosition(new Vector3(float.Parse(x[0]), float.Parse(x[1])));
     }
-    public void SetPosition(Vector3 vector) { positionDestination = vector; }
+
+    public void SetPosition(Vector3 vector) {
+        positionDestination = vector;
+    }
+
+    void Start() {
+        float __x = 180;
+        float __y = 180;
+
+        if (_vertical == BubbleVertical.Top) {
+            __x = 0;
+        } if (_horizontal == BubbleHorizontal.Right) {
+            __y = 0;
+        }
+
+        _rectTransform.localRotation = _textRectTransform.localRotation =
+            Quaternion.Euler(__x, __y, 0);
+    }
+
+    void Update() {
+        if (changingRot) ChangeRotation();
+        if (changingPos) ChangePosition();
+    }
+
+    void ChangeRotation() {
+        if (rotationTime > 1) {
+            changingRot = false;
+            return;
+        }
+
+        if (rotationTime > .5f) {
+            _textRectTransform.localRotation = Quaternion.Euler(rotationTargetAngles);
+        }
+
+        _rectTransform.localRotation = Quaternion.Euler(
+            Vector3.Lerp(
+                rotationStartAngles,
+                rotationTargetAngles,
+                rotationTime
+            )
+        );
+
+        rotationTime += Time.deltaTime * _rotationSpeed;
+    }
+
+    void ChangePosition() {
+        if (positionTime > 1) {
+            changingPos = false;
+            return;
+        }
+
+        _rectTransform.localPosition = Vector3.Lerp(
+            positionStart,
+            positionTarget,
+            positionTime
+        );
+
+        positionTime += Time.deltaTime * _positionSpeed;
+    }
 }
 
 [Serializable]
 [System.ComponentModel.DefaultValue(BubbleVertical.Bottom)]
-public enum BubbleVertical : byte
-{
+public enum BubbleVertical : byte {
     Bottom = 1,
     Top = 3,
 }
 
 [Serializable]
 [System.ComponentModel.DefaultValue(BubbleHorizontal.Left)]
-public enum BubbleHorizontal : byte
-{
+public enum BubbleHorizontal : byte {
     Left = 1,
     Right = 3
 }
