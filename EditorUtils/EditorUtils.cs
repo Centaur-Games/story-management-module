3fee51cbb285a9522437ac4519fa1bbedcb4e871
EnesNder <ensnder349@gmail.com>
2023-04-08 03:44:07 +0300

    Editor Utils take screenshot methods added

--- EditorUtils/EditorUtils.cs
+++ EditorUtils/EditorUtils.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,6 +10,9 @@ public class EditorUtils : MonoBehaviour
     [MenuItem("Centaur Games/Take ScreenShot")]
     public static void takeScreenshot() {
         Guid uid = System.Guid.NewGuid();
-        ScreenCapture.CaptureScreenshot($"{uid}.jpg");
+        if(!Directory.Exists("Assets/Images/Screenshoots")) Directory.CreateDirectory("Assets/Images/Screenshoots/");
+        ScreenCapture.CaptureScreenshot($"Assets/Images/Screenshoots/{uid}.jpg");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
     }
 }
