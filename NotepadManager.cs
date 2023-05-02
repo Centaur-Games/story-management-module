16ad6cae58818c56482132bfe567e3ca817cb9de
Atakan Karagül <ata2014kara2@gmail.com>
2023-05-02 22:03:02 +0300

    NotepadManager: fix stuck locking of validatables
    locking was getting stuck when popped back and pushed, fixed now.
    Also paging and other stuff are fixed

--- NotepadManager.cs
+++ NotepadManager.cs
@@ -8,6 +8,7 @@ public class NotepadManager : MonoBehaviour {
 
     public int pageCount { get => transform.childCount; }
     int currPage = 0;
+    int maxPage = 0;
 
     int traversalDepth = 0;
     List<(bool open, GameObject obj)> childrenList = new();
@@ -53,6 +54,16 @@ public class NotepadManager : MonoBehaviour {
         throw new System.Exception("obj not found");
     }
 
+    public int getChildrenIndex(GameObject obj) {
+        for (int i=0; i < childrenList.Count; i++) {
+            if (obj == childrenList[i].obj) {
+                return i;
+            }
+        }
+
+        throw new System.Exception("children not found");
+    }
+
     public void openUntilObj(GameObject obj) {
         GetOpenStatus(obj);
 
@@ -60,7 +71,10 @@ public class NotepadManager : MonoBehaviour {
 
         for (int i=0; i < childrenList.Count; i++) {
             childrenList[i] = (open, childrenList[i].obj);
-            if (childrenList[i].obj == obj) open = false;
+
+            if (childrenList[i].obj == obj) {
+                open = false;
+            }
         }
 
         ReloadPage();
@@ -78,19 +92,59 @@ public class NotepadManager : MonoBehaviour {
         ReloadPage();
     }
 
+    public void nextPage() {
+        if (currPage >= maxPage) return;
+        switchPage(currPage+1);
+    }
+
+    public void prevPage() {
+        if (currPage <= 0) return;
+        switchPage(currPage-1);
+    }
+
     public void switchPage(int pageIndex) {
-        if (pageIndex > pageCount || pageIndex < 0) {
+        if (pageIndex > pageCount || pageIndex < -1) {
             throw new System.Exception("page index exceeds page count");
         }
 
-        currPage = pageIndex;
+        if (pageIndex > maxPage) {
+            maxPage = pageIndex;
+        }
+
+        currPage = pageIndex == -1 ? transform.childCount-1 : pageIndex;
         ReloadPage();
     }
 
-    public void ReloadPage() {
-        foreach(var child in childrenList) {
-            child.obj.SetActive(child.open);
+    public void closeAllChildren() {
+        foreach (var child in childrenList) {
+            child.obj.SetActive(false);
         }
+    }
+
+    IEnumerable<(bool open, GameObject obj)> matchPage(int pageIndex) {
+        var pageTransform = childOf(transform.GetChild(pageIndex));
+
+        foreach (var pchild in pageTransform) {
+            foreach (var child in childrenList) {
+                if (child.obj == pchild.gameObject) {
+                    yield return child;
+                }
+            }
+        }
+    }
+
+    public void ReloadPage() {
+        closeAllChildren();
+        lockAllCheckables();
+
+        foreach (var element in matchPage(currPage)) {
+            Debug.Log($"setting element: ${element.obj.name} : ${element.open}");
+            element.obj.SetActive(element.open);
+        }
+
+        unLockLastPageCheckables();
+
+        transform.GetChild(currPage).gameObject.SetActive(true);
     }
 
     public void doAutoCheck() {
@@ -131,5 +185,73 @@ public class NotepadManager : MonoBehaviour {
         }
 
         depth--;
+    }
+
+    void lockAllCheckables() {
+        lockAllCheckables(transform);
+    }
+
+    void unLockLastPageCheckables() {
+        (bool open, GameObject obj)? lastobj = null;
+
+        foreach (var child in matchPage(maxPage)) {
+            if (!child.open && (lastobj?.open ?? false)) {
+                break;
+            }
+
+            lastobj = child;
+        }
+
+        if (lastobj != null) {
+            openLastNthCheckable(lastobj.Value, 1);
+        }
+    }
+
+    void lockAllCheckables(Transform obj) {
+        foreach (var child in childOf(obj)) {
+            var p = child.GetComponent<IInputValidateable>();
+
+            if (p != null) {
+                p.locked = true;
+            }
+
+            lockAllCheckables(child.transform);
+        }
+    }
+
+    void openLastNthCheckable((bool open, GameObject obj) obj, int cnt) {
+        var childIndex = childrenList.IndexOf(obj);
+
+        if (childIndex == -1) {
+            throw new System.Exception("child not found");
+        }
+
+        int x=0;
+        for (int i = childIndex; i >= 0; i--) {
+            if (!_openLastNthCheckable(childrenList[i].obj.transform, ref x, cnt)) {
+                return;
+            }
+        }
+    }
+
+    bool _openLastNthCheckable(Transform obj, ref int cnt, int target) {
+        foreach (var child in childOf(obj)) {
+            if (cnt >= target) {
+                return false;
+            }
+
+            var p = child.GetComponent<IInputValidateable>();
+
+            if (p != null) {
+                p.locked = false;
+
+                Debug.Log($"opening: {child.name}");
+                cnt++;
+            }
+
+            _openLastNthCheckable(child.transform, ref cnt, target);
+        }
+
+        return true;
     }
 }
