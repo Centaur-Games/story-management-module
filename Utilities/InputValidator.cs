1e5320c543b93cedba5e96e3d922aef3312aa457
Atakan Karagül <ata2014kara2@gmail.com>
2023-05-17 00:19:23 +0300

    InputValidator: do field var init on set or get if null

--- Utilities/InputValidator.cs
+++ Utilities/InputValidator.cs
@@ -1,21 +1,34 @@
 using UnityEngine;
 
+[RequireComponent(typeof(TMPro.TMP_InputField))]
 public class InputValidator : MonoBehaviour, IInputValidateable {
     [SerializeField] string correctAns;
     private bool _locked = false;
     public bool locked { get => _locked; set {
+        InitializeField();
         field.interactable = !value;
         _locked = value;
     }}
 
     TMPro.TMP_InputField field;
 
+    void InitializeField() {
+        if (field == null && !TryGetComponent<TMPro.TMP_InputField>(out field)) {
+            throw new System.Exception(
+                $"InputValidator put on a non inputfield object obj: {gameObject.name}"
+            );
+        }
+    }
+
     void Start() {
-        field = GetComponent<TMPro.TMP_InputField>();
+        InitializeField();
     }
 
     public bool correct {
-        get => field.text == correctAns;
+        get {
+            InitializeField();
+            return field.text == correctAns;
+        }
     }
 }
 
