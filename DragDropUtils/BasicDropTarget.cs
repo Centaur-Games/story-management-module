044a1a1591d3cc1e4bf905b6b4b1b9b924202faf
Atakan Karagül <ata2014kara2@gmail.com>
2023-05-17 04:33:05 +0300

    BasicDropTarget: generic event based drop target

---
+++ DragDropUtils/BasicDropTarget.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BasicDropTarget : MonoBehaviour, IDropTargetListener {
+    [SerializeField] Dragable targetDragable;
+    [SerializeField] UnityEvent onSuccess;
+    [SerializeField] UnityEvent onFail;
+    [SerializeField] UnityEvent<Dragable> onHover;
+
+    void IDropTargetListener.OnDrop(Dragable dragable) {
+        if (dragable == targetDragable) {
+            onSuccess.Invoke();
+        } else {
+            onFail.Invoke();
+        }
+
+        throw new DragableRejected();
+    }
+
+    void IDropTargetListener.OnHoverEnter(Dragable dragable) {
+        onHover.Invoke(dragable);
+    }
+
+    void IDropTargetListener.OnDropCancel(Dragable dragable) {}
+    void IDropTargetListener.OnElementDragCancel(Dragable dragable) {}
+    void IDropTargetListener.OnElementDragEnd(Dragable dragable, DropTarget droppedTarget) {}
+    void IDropTargetListener.OnElementDragStart(Dragable dragable) {}
+    void IDropTargetListener.OnHoverExit(Dragable dragable) {}
+    void IDropTargetListener.OnHoverStay(Dragable dragable) {}
+}
