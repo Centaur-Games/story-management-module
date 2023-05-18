a629494b3a1241649da90e5fb35cb037b66e1619
Atakan Karagül <ata2014kara2@gmail.com>
2023-05-18 21:06:55 +0300

    Pourable & PourTarget: implementation Generalized version of sanford's pouring

---
+++ DragDropUtils/PourTarget.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PourTarget : MonoBehaviour, IDropTargetListener {
+    [SerializeField] float reserveFillAmount;
+    [SerializeField] Image reserve;
+    [SerializeField] List<Pourable> acceptedPourables;
+
+    [SerializeField] UnityEvent invokeOnFill;
+    [SerializeField] float fillEpsilon = 0.05f;
+
+    public bool CheckPoured() {
+        return (reserve.fillAmount+fillEpsilon) >= reserveFillAmount;
+    }
+
+    void IDropTargetListener.OnDrop(Dragable dragable) {
+        throw new DragableRejected();
+    }
+
+    void IDropTargetListener.OnDropCancel(Dragable dragable) {
+        if (CheckPoured()) {
+            invokeOnFill.Invoke();
+        }
+
+        if (dragable is Pourable && acceptedPourables.Contains((Pourable)dragable)) {
+            ((Pourable)dragable).StopPour();
+        }
+    }
+
+    void IDropTargetListener.OnHoverStay(Dragable dragable) {
+        if (dragable is Pourable && acceptedPourables.Contains((Pourable)dragable)) {
+            var amount = ((Pourable)dragable).pourReserveTick();
+            reserve.fillAmount += amount * reserveFillAmount;
+        }
+    }
+
+    void IDropTargetListener.OnHoverEnter(Dragable dragable) {
+        if (dragable is Pourable && acceptedPourables.Contains((Pourable)dragable)) {
+            ((Pourable)dragable).StartPour();
+        }
+    }
+
+    void IDropTargetListener.OnHoverExit(Dragable dragable) {
+        if (dragable is Pourable && acceptedPourables.Contains((Pourable)dragable)) {
+            ((Pourable)dragable).StopPour();
+        }
+    }
+
+    void IDropTargetListener.OnElementDragCancel(Dragable dragable) {}
+    void IDropTargetListener.OnElementDragEnd
+        (Dragable dragable, DropTarget droppedTarget) {}
+    void IDropTargetListener.OnElementDragStart(Dragable dragable) {}
+}
---
+++ DragDropUtils/Pourable.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Pourable : Dragable {
+    [SerializeField] float pourRate;
+    [SerializeField] float reserveMaxFill;
+    [SerializeField] float secondaryPourReserveMaxFill;
+
+    [SerializeField] bool pourClockwise;
+
+    [SerializeField] GameObject uprightImage;
+    [SerializeField] Image secondaryPourReserve;
+
+    [SerializeField] GameObject sidewaysImage;
+    [SerializeField] Image pourReserve;
+
+    [SerializeField] GameObject pourLine;
+
+    Quaternion startAngle;
+    Quaternion secondaryStartAngle;
+    Quaternion pourLineStartAngle;
+
+    protected override void Start() {
+        base.Start();
+
+        secondaryStartAngle = transform.rotation;
+        startAngle = sidewaysImage.gameObject.transform.rotation;
+        pourLineStartAngle = pourLine.gameObject.transform.rotation;
+    }
+
+    public float pourReserveTick() {
+        float fillDelta = (Time.deltaTime * pourRate);
+        float relativeFillDelta = fillDelta * reserveMaxFill;
+
+        if (pourReserve.fillAmount - relativeFillDelta < 0) {
+            pourLine.SetActive(false);
+            return 0;
+        }
+
+        pourReserve.fillAmount -= relativeFillDelta;
+        float angleDelta = (pourClockwise ? -1 : 1) * (relativeFillDelta * 90);
+
+        sidewaysImage.transform.rotation *= Quaternion.AngleAxis(angleDelta, Vector3.forward);
+        pourLine.transform.rotation *=
+            Quaternion.AngleAxis(-angleDelta, Vector3.forward);
+
+        return fillDelta;
+    }
+
+    public void StartPour() {
+        pourLine.SetActive(true);
+        sidewaysImage.SetActive(true);
+        uprightImage.SetActive(false);
+    }
+
+    public void StopPour() {
+        secondaryPourReserve.fillAmount =
+            pourReserve.fillAmount / reserveMaxFill;
+
+        pourLine.SetActive(false);
+        sidewaysImage.SetActive(false);
+        uprightImage.SetActive(true);
+    }
+
+    public void resetPourReserve() {
+        pourReserve.fillAmount = reserveMaxFill;
+        secondaryPourReserve.fillAmount = secondaryPourReserveMaxFill;
+
+        transform.rotation = secondaryStartAngle;
+        sidewaysImage.transform.rotation = startAngle;
+        pourLine.transform.rotation = pourLineStartAngle;
+    }
+}
