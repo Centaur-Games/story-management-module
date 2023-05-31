42b7b8ef63a56e49d6aeec7eda9a4cb961add057
EnesNder <ensnder349@gmail.com>
2023-05-31 23:36:58 +0300

    New methods added to PreparedAnimEvents

--- PreparedAnims/PreparedAnimEvents.cs
+++ PreparedAnims/PreparedAnimEvents.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class PreparedAnimEvents : MonoBehaviour
 {
+    Animator animator;
+    void Awake() { getAnimator(); }
+
+    public void forward() {
+        getAnimator();
+        animator.Play("forward");
+    }
+    public void revert() {
+        getAnimator();
+        animator.Play("revert");
+    }
+
     public void pushState() {
+        getAnimator();
+        animator.Play("idle");
         StoryManager.pushNextState(false);
     }
 
     public void prevState() {
+        getAnimator();
+        animator.Play("idle");
         StoryManager.backStory(1);
+    }
+
+    void getAnimator() {
+        if(animator != null) return;
+        animator = GetComponent<Animator>();
     }
 }
