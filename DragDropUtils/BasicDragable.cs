65f79f8ee53fbe6d6b40f6bf5ba31988f2c0b087
EnesNder <ensnder349@gmail.com>
2023-06-03 05:37:55 +0300

    Basic dragable drag/drop scale feature added

--- DragDropUtils/BasicDragable.cs
+++ DragDropUtils/BasicDragable.cs
@@ -5,18 +5,39 @@ using UnityEngine.Events;
 
 public class BasicDragable : Dragable
 {
+    RectTransform rect;
+
+    [Space]
+    public float DragScale = 1;
+    public float DropScale = 1;
+    [Space]
+
     [SerializeField] UnityEvent onDragCallbacks;
     [SerializeField] UnityEvent onDropCallbacks;
+
+    protected override void Start()
+    {
+        base.Start();
+        if(active) setRectScale(DropScale);
+    }
 
     public override void OnDrag(GameObject target, DropTarget drop)
     {
         base.OnDrag(target, drop);
         onDragCallbacks.Invoke();
+        setRectScale(DragScale);
     }
 
     public override void OnDrop(DropTarget target)
     {
         base.OnDrop(target);
         onDropCallbacks.Invoke();
+        if(active) setRectScale(DropScale);
+    }
+
+    void setRectScale(float scale) {
+        if(rect == null) rect = GetComponent<RectTransform>();
+        if(rect == null) return;
+        rect.localScale = Vector3.one * scale;
     }
 }
